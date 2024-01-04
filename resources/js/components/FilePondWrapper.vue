fda39b053fbcc5ce18c1e2734503c684b352101f
Keko-94 <62820283+Keko-94@users.noreply.github.com>
2024-01-04 18:33:38 +0200

    Update FilePondWrapper.vue

--- resources/js/components/FilePondWrapper.vue
+++ resources/js/components/FilePondWrapper.vue
@@ -36,192 +36,191 @@
 
 <script>
 
-    import { ref } from 'vue'
-    import cloneDeep from 'lodash/cloneDeep'
-    import vueFilePond from 'vue-filepond'
-    import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
-    import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
-    import FilePondPluginMediaPreview from 'filepond-plugin-media-preview'
-    import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
-    import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
-    import FilePondPluginGetFile from 'filepond-plugin-get-file'
+import { ref } from 'vue'
+import cloneDeep from 'lodash/cloneDeep'
+import vueFilePond from 'vue-filepond'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import FilePondPluginMediaPreview from 'filepond-plugin-media-preview'
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
+import FilePondPluginGetFile from 'filepond-plugin-get-file'
 
-    import 'filepond/dist/filepond.min.css'
-    import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css'
-    import 'filepond-plugin-media-preview/dist/filepond-plugin-media-preview.css'
-    import 'filepond-plugin-get-file/dist/filepond-plugin-get-file.min.css'
+import 'filepond/dist/filepond.min.css'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css'
+import 'filepond-plugin-media-preview/dist/filepond-plugin-media-preview.css'
+import 'filepond-plugin-get-file/dist/filepond-plugin-get-file.min.css'
 
-    export default {
-        components: { FilePond },
-        props: [ 'field', 'resourceName', 'onChange', 'errors', 'columns', 'limit', 'allowReorder', 'allowRemove', 'mode', 'action' ],
-        setup(props) {
+export default {
+    props: [ 'field', 'resourceName', 'onChange', 'errors', 'columns', 'limit', 'allowReorder', 'allowRemove', 'mode', 'action' ],
+    setup(props) {
 
-            const plugins = [
-                FilePondPluginImageExifOrientation,
-                FilePondPluginFileValidateType,
-                FilePondPluginFileValidateSize,
-                FilePondPluginImagePreview,
-                FilePondPluginMediaPreview,
-            ]
+        const plugins = [
+            FilePondPluginImageExifOrientation,
+            FilePondPluginFileValidateType,
+            FilePondPluginFileValidateSize,
+            FilePondPluginImagePreview,
+            FilePondPluginMediaPreview,
+        ]
 
-            const isPreviewEnabled = props.field.preview === undefined ? true : props.field.preview
+        const isPreviewEnabled = props.field.preview === undefined ? true : props.field.preview
 
-            if (props.field.downloadable && isPreviewEnabled) {
-                plugins.push(FilePondPluginGetFile)
-            }
+        if (props.field.downloadable && isPreviewEnabled) {
+            plugins.push(FilePondPluginGetFile)
+        }
 
-            const component = vueFilePond(...plugins)
+        const component = vueFilePond(...plugins)
 
-            return {
-                component,
-                isPreviewEnabled,
-                instance: ref(),
-                nameField: props.field.attribute,
-                files: cloneDeep(props.field.value),
-                cssVars: {
-                    '--filepond-column': (100 / (props.columns || props.field.columns)) + '%',
-                    '--filepond-max-height': props.field.maxHeight,
-                },
-                serverOptions: {
-                    url: '/nova-vendor/nova-filepond',
-                    revert: '/revert',
-                    load: `/load?serverId=`,
-                    process: {
-                        url: '/process',
-                        ondata: formData => {
+        return {
+            component,
+            isPreviewEnabled,
+            instance: ref(),
+            nameField: props.field.attribute,
+            files: cloneDeep(props.field.value),
+            cssVars: {
+                '--filepond-column': (100 / (props.columns || props.field.columns)) + '%',
+                '--filepond-max-height': props.field.maxHeight,
+            },
+            serverOptions: {
+                url: '/nova-vendor/nova-filepond',
+                revert: '/revert',
+                load: `/load/?serverId=`,
+                process: {
+                    url: '/process',
+                    ondata: formData => {
 
-                            if (props.mode === 'action-modal') {
-                                formData.append('action', props.action)
-                            }
+                        if (props.mode === 'action-modal') {
+                            formData.append('action', props.action)
+                        }
 
-                            formData.append('attribute', props.field.attribute)
-                            formData.append('resourceName', props.resourceName)
+                        formData.append('attribute', props.field.attribute)
+                        formData.append('resourceName', props.resourceName)
 
-                            return formData
-                        },
-                        onerror: errors => {
-                            props.errors.record(JSON.parse(errors))
-                        },
+                        return formData
                     },
-                    headers: {
-                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+                    onerror: errors => {
+                        props.errors.record(JSON.parse(errors))
                     },
                 },
+                headers: {
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+                },
             }
+        }
 
-        },
+    },
 
-    }
+}
 
 </script>
 
 <style lang="scss">
 
-    :root {
-        --filepond-column: 100%;
-        --filepond-max-height: auto;
-    }
+:root {
+    --filepond-column: 100%;
+    --filepond-max-height: auto;
+}
 
-    .dark {
-
-        .filepond--drop-label {
-            color: rgba(var(--colors-gray-400));
-        }
-
-        .filepond--panel-root {
-            background-color: rgba(var(--colors-gray-900));
-            border-color: rgba(var(--colors-gray-700));
-        }
-
-        .filepond--image-preview-wrapper {
-            border-color: rgba(var(--colors-gray-700));
-        }
-
-        .filepond--image-preview {
-            background-color: rgba(var(--colors-gray-800));
-        }
-
-        .filepond--list-scroller::-webkit-scrollbar-thumb {
-            background-color: rgba(var(--colors-gray-700));
-        }
-
-        .filepond--image-preview-overlay {
-            color: rgba(var(--colors-gray-400))
-        }
-
-        .filepond--item-panel {
-            background: rgba(var(--colors-gray-800));
-        }
-
-    }
-
-    [data-filepond-item-state*='error'] .filepond--item-panel,
-    [data-filepond-item-state*='invalid'] .filepond--item-panel {
-        background-color: rgba(var(--colors-red-500));
-    }
-
-    [data-filepond-item-state='processing-complete'] .filepond--item-panel {
-        background-color: rgba(var(--colors-green-500));
-    }
-
-    .filepond--file-wrapper {
-        background: transparent;
-    }
-
-    .filepond--item-panel {
-        background: rgba(var(--colors-gray-400));
-    }
-
-    .filepond--image-preview-overlay {
-
-        &.filepond--image-preview-overlay-idle {
-            color: rgba(var(--colors-gray-500));
-        }
-
-        &.filepond--image-preview-overlay-success {
-            color: rgba(var(--colors-green-500));
-        }
-
-        &.filepond--image-preview-overlay-failure {
-            color: rgba(var(--colors-red-500));
-        }
-    }
-
-    .filepond--image-preview {
-        background-color: rgba(var(--colors-gray-100));
-    }
-
-    .filepond--image-preview-wrapper {
-        border-color: rgba(var(--colors-gray-300));
-    }
+.dark {
 
     .filepond--drop-label {
-        color: rgba(var(--colors-gray-600))
-    }
-
-    .filepond--root {
-        max-height: var(--filepond-max-height);
+        color: rgba(var(--colors-gray-400));
     }
 
     .filepond--panel-root {
-        border-radius: .25rem;
-        border-width: 1px;
-        border-color: rgba(var(--colors-gray-300));
-        background-color: rgba(var(--colors-gray-100));
+        background-color: rgba(var(--colors-gray-900));
+        border-color: rgba(var(--colors-gray-700));
     }
 
     .filepond--image-preview-wrapper {
-        border-radius: .25rem;
-        border-width: 1px;
+        border-color: rgba(var(--colors-gray-700));
     }
 
-    .filepond--fullsize-overlay {
-        position: fixed;
-        z-index: 20;
+    .filepond--image-preview {
+        background-color: rgba(var(--colors-gray-800));
     }
 
-    .filepond--item {
-        width: calc(var(--filepond-column) - .5em);
+    .filepond--list-scroller::-webkit-scrollbar-thumb {
+        background-color: rgba(var(--colors-gray-700));
     }
+
+    .filepond--image-preview-overlay {
+        color: rgba(var(--colors-gray-400))
+    }
+
+    .filepond--item-panel {
+        background: rgba(var(--colors-gray-800));
+    }
+
+}
+
+[data-filepond-item-state*='error'] .filepond--item-panel,
+[data-filepond-item-state*='invalid'] .filepond--item-panel {
+    background-color: rgba(var(--colors-red-500));
+}
+
+[data-filepond-item-state='processing-complete'] .filepond--item-panel {
+    background-color: rgba(var(--colors-green-500));
+}
+
+.filepond--file-wrapper {
+    background: transparent;
+}
+
+.filepond--item-panel {
+    background: rgba(var(--colors-gray-400));
+}
+
+.filepond--image-preview-overlay {
+
+    &.filepond--image-preview-overlay-idle {
+        color: rgba(var(--colors-gray-500));
+    }
+
+    &.filepond--image-preview-overlay-success {
+        color: rgba(var(--colors-green-500));
+    }
+
+    &.filepond--image-preview-overlay-failure {
+        color: rgba(var(--colors-red-500));
+    }
+}
+
+.filepond--image-preview {
+    background-color: rgba(var(--colors-gray-100));
+}
+
+.filepond--image-preview-wrapper {
+    border-color: rgba(var(--colors-gray-300));
+}
+
+.filepond--drop-label {
+    color: rgba(var(--colors-gray-600))
+}
+
+.filepond--root {
+    max-height: var(--filepond-max-height);
+}
+
+.filepond--panel-root {
+    border-radius: .25rem;
+    border-width: 1px;
+    border-color: rgba(var(--colors-gray-300));
+    background-color: rgba(var(--colors-gray-100));
+}
+
+.filepond--image-preview-wrapper {
+    border-radius: .25rem;
+    border-width: 1px;
+}
+
+.filepond--fullsize-overlay {
+    position: fixed;
+    z-index: 20;
+}
+
+.filepond--item {
+    width: calc(var(--filepond-column) - .5em);
+}
 
 </style>
